3ae2206dd3b6b4ce542b3cb19b5480d83b18faaf
premsjce <premsjce@gmail.com>
2014-10-04 02:47:06 +0530

    Moving rails blogger app folder for chapter I0

--- RailsBloggerApp/config/routes.rb
+++ RailsBloggerApp/config/routes.rb
@@ -1,3 +1,3 @@
-Rails.application.routes.draw do
-  resources :articles
+RailsBloggerApp::Application.routes.draw do
+    resources :articles
 end
--- RailsBloggerApp/db/schema.rb
+++ RailsBloggerApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927153452) do
+ActiveRecord::Schema.define(version: 20141003062227) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
